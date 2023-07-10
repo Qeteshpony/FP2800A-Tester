66abdb034dc38ca23341db6be017dc87a18b23f6
Qetesh <27067788+Qeteshpony@users.noreply.github.com>
2023-07-10 10:54:21 +0200

    fix: turn the loops around
    Making the loops count down instead of up to make the LED light up in a nicer order. Also added some comments

--- arduino/FP2800A-Tester/FP2800A-Tester.ino
+++ arduino/FP2800A-Tester/FP2800A-Tester.ino
@@ -10,7 +10,7 @@
 
 void setup() {
   // start serial 
-  Serial.begin(2000000);
+  Serial.begin(2000000);  //this speed only works with Arduino IDE 2.x or an external serial monitor. Use 115200 for older IDEs
 
   // initialize pins
   pinMode(FPA0, OUTPUT);
@@ -20,28 +20,29 @@ void setup() {
   pinMode(FPB1, OUTPUT);
   pinMode(FPDATA, OUTPUT);
   pinMode(FPENABLE, OUTPUT);
-  pinMode(COM, INPUT);  // not used yet
+  pinMode(COM, INPUT);  // only needed as output if the COM pin on the FP2800A doesn't work 
 }
 
 void loop() {
-  // Toggle data pin
+  // Toggle data pin for each loop so we switch between positive and negative output
   bool data = !digitalRead(FPDATA);
   digitalWrite(FPDATA, data);
 
   // Loop through all the outputs and enable one after another
   int a, b;
   char buf[10];
-  for (b = 0; b <= 3; b++) {
+  for (b = 3; b >= 0; b--) {  
     digitalWrite(FPB0, (b >> 0) % 2);
     digitalWrite(FPB1, (b >> 1) % 2);
-    for (a = 1; a <= 7; a++) {
-      sprintf(buf, "%d, %d = %d", b, a, data);
+    for (a = 7; a >= 1; a--) {
+      sprintf(buf, "%d, %d = %d", b, a, data);  
       Serial.println(buf);
       digitalWrite(FPA0, (a >> 0) % 2);
       digitalWrite(FPA1, (a >> 1) % 2);
       digitalWrite(FPA2, (a >> 2) % 2);
       digitalWrite(FPENABLE, 1);
-      delay(250);
+      delayMicroseconds(100);  // use for inspection of all pins at once
+      //delay(250); // use for visual inspection of each output
       digitalWrite(FPENABLE, 0);
     }
   }
